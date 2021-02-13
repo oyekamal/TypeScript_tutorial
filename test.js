364fa46781c15d23d2f98cc43c80e1ad5ab0c810
oykamal <autocratickamal1@gmail.com>
2021-02-13 09:53:02 +0500

    first hello world file

---
+++ test.js
@@ -0,0 +1,4 @@
+var message = "hello World";
+var about = 'My name is kamal, we are learning TypeScript';
+console.log(message);
+console.log(about);
---
+++ test.ts
@@ -0,0 +1,4 @@
+var message:string = "hello World"
+var about: string = 'My name is kamal, we are learning TypeScript'
+console.log(message)
+console.log(about)
