d3a78f3505f899d7dea555ac3aea24c3df196c8a
oykamal <autocratickamal1@gmail.com>
2021-02-13 10:05:47 +0500

    variable

---
+++ variable/variable.js
@@ -0,0 +1,8 @@
+var name_ = "kamal";
+var num1 = 12;
+var num2 = 11;
+var total = num1 + num2;
+console.log("my name is :" + name_);
+console.log("first number : " + num1);
+console.log("second number : " + num2);
+console.log("totel : " + total);
---
+++ variable/variable.ts
@@ -0,0 +1,8 @@
+var name_:string = "kamal";
+var num1: number = 12;
+var num2: number = 11;
+var total = num1 + num2;
+console.log("my name is :"+ name_)
+console.log("first number : "+ num1)
+console.log("second number : "+ num2)
+console.log("totel : "+ total)
