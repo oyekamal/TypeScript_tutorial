742d3cbeebb6224df5584fec33b9e6181260884e
oykamal <autocratickamal1@gmail.com>
2021-02-13 10:16:02 +0500

    operations

---
+++ Operator/Operator.js
@@ -0,0 +1,14 @@
+var a = 100;
+var b = 200;
+console.log(a + b);
+console.log(a - b);
+console.log(a < b);
+console.log(a == b);
+console.log(a != b);
+a++;
+console.log(a);
+b--;
+console.log(b--);
+var num = -2;
+var result = num < 0 ? "negative" : "positive";
+console.log(result);
---
+++ Operator/Operator.ts
@@ -0,0 +1,16 @@
+let a = 100
+let b = 200
+
+console.log(a+b)
+console.log(a-b)
+console.log(a<b)
+console.log(a==b)
+console.log(a!=b)
+a++
+console.log(a)
+b--
+console.log(b--)
+
+let num = -2;
+let result: string = num < 0 ? "negative": "positive"
+console.log(result)
